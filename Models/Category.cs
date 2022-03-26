4867147bdb7bdd85ee86ae48ba9e9bdbf3e942f6
skg14-dev <sean.kg@yp-uae.com>
2022-03-26 15:39:17 +0400

    Models and db context

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HPlusSportApi.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public virtual List<Product> Products { get; set; } 
+    }
+}
---
+++ Models/ShopContext.cs
@@ -0,0 +1,26 @@
+﻿using HPlusSportApi.Models.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPlusSportApi.Models
+{
+
+    public class ShopContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Products)
+                .WithOne(c => c.Category)
+                .HasForeignKey(a => a.CategoryId);
+
+            modelBuilder.Seed();
+        }
+    }
+
+}
