d1677e4b72264259f2e759a6cf23d27b72983895
skg14-dev <sean.kg@yp-uae.com>
2022-03-26 15:38:22 +0400

    Products controller get all and single

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using HPlusSportApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPlusSportApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext shopContext;
+
+        public ProductsController(ShopContext shopContext)
+        {
+            this.shopContext=shopContext;
+            this.shopContext.Database.EnsureCreated();
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAllProducts()
+        {
+            var products = await shopContext.Products.ToListAsync();
+            if (!products.Any())
+            {
+                return NotFound();
+            }
+            return Ok(products);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await shopContext.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
